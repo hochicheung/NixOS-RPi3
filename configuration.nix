c49398f7ba0897c00e1f829394d7c9cee6019cde
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-05-08 21:17:41 +0200

    copy over config settings

--- configuration.nix
+++ configuration.nix
@@ -3,6 +3,8 @@
 	# NixOS wants to enable GRUB by default
 	boot.loader.grub.enable = false;
 
+	networking.hostName = "raspi3";
+
 	# Enables the generation of /boot/extlinux/extlinux.conf
 	boot.loader.generic-extlinux-compatible.enable = true;
 
@@ -22,4 +24,42 @@
 	};
 
 	swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+
+	# User Management
+	users.users.samcheung = {
+		isNormalUser = true;
+		home = "/home/samcheung";
+		extraGroups = ["wheel" "networkmanager"];
+	};
+
+	# Nix garbage collector
+	nix.gc.automatic = true;
+
+	# Don't use swap unless ram is full
+	boot.kernel.sysctl = {
+		"vm.swappiness" = 0;
+	};
+	services.fstrim.enable = true;
+
+	# Clean /tmp on boot
+	boot.cleanTmpDir = true;
+
+	# Time & Date
+	time.timeZone = "Europe/Stockholm";
+	services.localtime.enable = true;
+
+	# Networking
+	networking.networkmanager.enable = true;
+
+	# Auto-upgrade
+	system.autoUpgrade.enable = true;
+	system.autoUpgrade.allowReboot = true;
+
+	# Allow Unfree
+	nixpkgs.config.allowUnfree = true;
+
+	environment.systemPackages = with pkgs; [
+		git
+		vim
+	];
 }
