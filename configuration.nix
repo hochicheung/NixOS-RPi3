2d6f41cdbf3d6dd486aa804d9272cd09f1b1ca83
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-07-08 19:46:51 +0200

    minimal branch

--- configuration.nix
+++ configuration.nix
@@ -6,30 +6,6 @@
 	# Hostname
 	networking.hostName = "raspi3";
 
-	# Enables the generation of /boot/extlinux/extlinux.conf
-	boot.loader.generic-extlinux-compatible.enable = true;
-
-	#	 !!! If your board is a Raspberry Pi 3, select not latest (5.8 at the time)
-	#	 !!! as it is currently broken (see https://github.com/NixOS/nixpkgs/issues/97064)
-	boot.kernelPackages = pkgs.linuxPackages;
-
-	# !!! Needed for the virtual console to work on the RPi 3, as the default of 16M doesn't seem to be enough.
-	boot.kernelParams = ["cma=32M"];
-
-	# File systems configuration for using the installer's partition layout
-	fileSystems = {
-		"/" = {
-			device = "/dev/disk/by-label/NIXOS_SD";
-			fsType = "ext4";
-		};
-	};
-
-	# Add Swap
-	swapDevices = [ { device = "/swapfile"; size = 1024; } ];
-
-	# Load wifi device
-	hardware.enableRedistributableFirmware = true;
-
 	# User Management
 	users.users.samcheung = {
 		isNormalUser = true;
