034cb5e18af205d1fa047a4ddff4f13ba502bbc4
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-07-09 17:23:59 +0200

    latest

--- configuration.nix
+++ configuration.nix
@@ -3,16 +3,24 @@
 	# NixOS wants to enable GRUB by default
 	boot.loader.grub.enable = false;
 
-	# Hostname
-	networking.hostName = "raspi3";
+	# Kernel options
+	boot.kernelPackages = pkgs.linuxPackages_5_4;
 
-	# User Management
-	users.users.samcheung = {
-		isNormalUser = true;
-		home = "/home/samcheung";
-		extraGroups = ["wheel" "networkmanager"];
+	boot.loader.raspberryPi.enable = true;
+	boot.loader.raspberryPi.version = 3;
+	boot.loader.raspberryPi.uboot.enable = true;
+
+	# Filesystem configuration
+	fileSystems = {
+	  "/" = {
+		  device = "/dev/disk/by-label/NIXOS_SD;
+			fsType = "ext4";
+		};
 	};
-
+	
+	# Swap
+	swapDevices = [ { device = "/swapfile"; size = 1024;}];
+	
 	# Nix garbage collector
 	nix.gc.automatic = true;
 
@@ -25,28 +33,40 @@
 	# Clean /tmp on boot
 	boot.cleanTmpDir = true;
 
+	# User Management
+	users.users.samcheung = {
+		isNormalUser = true;
+		home = "/home/samcheung";
+		extraGroups = ["wheel" "networkmanager"];
+	};
+
 	# Time & Date
 	time.timeZone = "Europe/Stockholm";
 	services.localtime.enable = true;
 
 	# Networking
-	networking.networkmanager.enable = true;
+	networking = {
+	  hostName = raspi-nix;
+	  networkmanager.enable = true;
+		
+		firewall.enable = true;
+		firewall.allowedTCPPorts = [80 443 461];
+	}
 
 	# Auto-upgrade
 	system.autoUpgrade.enable = true;
 	system.autoUpgrade.allowReboot = true;
 
-	# Allow Unfree
-	nixpkgs.config.allowUnfree = true;
-
 	# Packages
 	environment.systemPackages = with pkgs; [
 		git
 		vim
+		libraspberrypi
 	];
 
 	# SSH
 	services.openssh.enable = true;
 	services.openssh.permitRootLogin = "yes";
 	services.openssh.ports = [461];
+	
 }
